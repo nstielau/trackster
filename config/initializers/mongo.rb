29f3c1b4ff5fe9c98842e37eaa88c437a33f5e02
Nick Stielau <nick.stielau@gmail.com>
2010-05-15 16:21:40 -0700

    Adding prod db configs.

--- config/initializers/mongo.rb
+++ config/initializers/mongo.rb
@@ -2,7 +2,7 @@ if Rails.env == "development"
   MongoMapper.connection = Mongo::Connection.new
   MongoMapper.database = "motionxdb"
 else
-  MongoMapper.connection =Mongo::Connection.new("flame.mongohq.com", "21160")
-  MongoMapper.database = "shopify-slideshows"
+  MongoMapper.connection =Mongo::Connection.new("flame.mongohq.com", "27066")
+  MongoMapper.database = "motionx"
   MongoMapper.database.authenticate("nstielau", "manifest")
 end
