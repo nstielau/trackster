4982fc35b5cfacfbc52df6b30725f7ecbe8eab24
Nick Stielau <nick.stielau@gmail.com>
2010-05-15 23:58:16 -0700

    Adding dashboard to homepage.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,7 @@
 class WelcomeController < ApplicationController
   def index
+    @total_distance = current_user ? current_user.total_distance : nil
+    @total_active_time = current_user ? current_user.total_active_time : nil
+    @total_tracks = current_user ? current_user.tracks.size : nil
   end
 end
