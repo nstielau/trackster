c29d62a4086c0c39294d5a16f4cd1583b5c1c86e
Nick Stielau <nick.stielau@gmail.com>
2010-05-15 22:08:25 -0700

    Correctly misspelled keys.

--- app/models/track.rb
+++ app/models/track.rb
@@ -1,6 +1,7 @@
 require 'zip/zip'
 require 'cobravsmongoose'
 require 'open-uri'
+require 'uri'
 
 class Track
   include MongoMapper::Document
@@ -23,20 +24,21 @@ class Track
   key :formatted_distance
   key :formatted_elapsed_time
   key :formatted_end_date
-  key :formattted_location_finish_lat
-  key :formattted_location_finish_lon
-  key :formattted_location_start_lat
-  key :formattted_location_start_lon
-  key :formattted_max_alt
-  key :formattted_max_speed
-  key :formattted_min_alt
-  key :formattted_start_date
+  key :formatted_location_finish_lat
+  key :formatted_location_finish_lon
+  key :formatted_location_start_lat
+  key :formatted_location_start_lon
+  key :formatted_max_alt
+  key :formatted_max_speed
+  key :formatted_min_alt
+  key :formatted_start_date
   key :note, String
   timestamps!
 
   belongs_to :user
 
   def self.parse_track(url)
+    url = parse_motionx_url_from_gmaps_url(url) if url.match("maps.google.com")
     file = open(url)
     result = read_motionx_zip_data(file.path)
     file.delete
@@ -44,6 +46,10 @@ class Track
   end
 
   private
+
+  def self.parse_motionx_url_from_gmaps_url(url)
+    URI::parse(url).query.split("&").select{|x| x.match("q=")}[0].sub("q=", "")
+  end
 
   def self.read_motionx_zip_data(file)
     result = nil
