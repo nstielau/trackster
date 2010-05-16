9f98125e27425aeb6f555feb41a34ca003c4e6fb
Nick Stielau <nick.stielau@gmail.com>
2010-05-16 01:27:14 -0700

    Adding a help page.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -4,4 +4,7 @@ class WelcomeController < ApplicationController
     @total_active_time = current_user ? current_user.total_active_time : nil
     @total_tracks = current_user ? current_user.tracks.size : nil
   end
+
+  def help
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -42,6 +42,7 @@ ActionController::Routing::Routes.draw do |map|
   #     admin.resources :products
   #   end
 
+  map.help '/help', :controller => 'welcome', :action => 'help'
   # You can have the root of your site routed with map.root -- just remember to delete public/index.html.
   map.root :controller => "welcome"
 
