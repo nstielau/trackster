000157e1e12c478fe5be232d4d9b5d492245c34e
Nick Stielau <nick.stielau@gmail.com>
2010-05-16 13:34:40 -0700

    Add test db env.

--- config/initializers/mongo.rb
+++ config/initializers/mongo.rb
@@ -3,6 +3,9 @@ require 'mongo_mapper'
 if Rails.env == "development"
   MongoMapper.connection = Mongo::Connection.new
   MongoMapper.database = "motionxdb"
+elsif Rails.env == "test"
+  MongoMapper.connection = Mongo::Connection.new
+  MongoMapper.database = "motionxdb_test"
 else
   MongoMapper.connection = Mongo::Connection.new("flame.mongohq.com", "27066")
   MongoMapper.database = "motionx"
