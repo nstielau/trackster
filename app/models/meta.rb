34e48a063d2eb2d72d27b84d29bd156ad2196745
Nick Stielau <nick.stielau@gmail.com>
2010-05-16 13:35:57 -0700

    Adding meta model to hold stateful info.

---
+++ app/models/meta.rb
@@ -0,0 +1,13 @@
+require 'mongo_mapper'
+
+class Meta
+  include MongoMapper::Document
+
+  key :last_twitter_update, Time, :default => Time.at(0)
+
+  def self.get_instance
+    raise "There should only ever be 1 meta" if Meta.count > 1
+    return Meta.create if Meta.count == 0
+    @meta ||= Meta.first
+  end
+end
