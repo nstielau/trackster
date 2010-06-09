dd894496a51087cc0b323ecf06c1c5d41d804835
Nick Stielau <nick.stielau@gmail.com>
2010-06-08 23:53:06 -0700

    Assigning emails to user.

--- app/models/mailman.rb
+++ app/models/mailman.rb
@@ -7,31 +7,46 @@ class Mailman
       inbox_count = gmail.inbox.count
       puts "There are #{inbox_count} emails in the inbox"
       gmail.inbox.emails(:from => "no-reply@motionx.com").each do |email|
-        puts "Email: #{email.inspect}"
+        begin
+          puts "Email: #{email.inspect}"
 
-        # save attachements
-        gpx_attachment = email.attachments.select{|a| a.filename.match("gpx")}.first
-        tmp_gpx_file = File.new(File.join(Dir.tmpdir, "track_#{Time.now.to_i}.gpx"), File::CREAT|File::RDWR)
-        tmp_gpx_file << gpx_attachment.read
+          # save attachements
+          gpx_attachment = email.attachments.select{|a| a.filename.match("gpx")}.first
+          tmp_gpx_file = File.new(File.join(Dir.tmpdir, "track_#{Time.now.to_i}.gpx"), File::CREAT|File::RDWR)
+          tmp_gpx_file << gpx_attachment.read
 
-        kmz_attachment = email.attachments.select{|a| a.filename.match("kmz")}.first
-        tmp_kmz_file = File.new(File.join(Dir.tmpdir, "track_#{Time.now.to_i}.kmz"), File::CREAT|File::RDWR)
-        tmp_kmz_file << kmz_attachment.read
+          kmz_attachment = email.attachments.select{|a| a.filename.match("kmz")}.first
+          tmp_kmz_file = File.new(File.join(Dir.tmpdir, "track_#{Time.now.to_i}.kmz"), File::CREAT|File::RDWR)
+          tmp_kmz_file << kmz_attachment.read
 
-        # Create track
-        track = Track.new(:kmz_file => tmp_kmz_file, :gpx_file => tmp_gpx_file)
-        track.save
-        track.update_from_kmz!
-        puts "Track: #{track.inspect}"
+          # Get User
+          user = Mailman.parse_user_from_address(email.to)
 
-        # Clean up tmp files
-        tmp_gpx_file.close
-        File.delete(tmp_gpx_file.path)
-        tmp_kmz_file.close
-        File.delete(tmp_kmz_file.path)
+          # Create track
+          track = Track.new(:kmz_file => tmp_kmz_file, :gpx_file => tmp_gpx_file, :user_id => user ? user.id : nil)
+          track.save
+          track.update_from_kmz!
+          puts "Track: #{track.inspect}"
 
-        email.archive!
+          # Clean up tmp files
+          tmp_gpx_file.close
+          File.delete(tmp_gpx_file.path)
+          tmp_kmz_file.close
+          File.delete(tmp_kmz_file.path)
+
+          email.archive!
+        rescue => e
+          puts "Error: "
+          puts e.inspect
+          e.backtrace.each{|l| puts "  #{l}"}
+        end
       end
+    end
+  end
+
+  def self.parse_user_from_address(address)
+    if matches = address.match(/(\w+)\+(\w+)@\w*\.\w*/)
+      User.first(:login => matches[2])
     end
   end
 end
--- test/unit/mailman_test.rb
+++ test/unit/mailman_test.rb
@@ -7,4 +7,16 @@ class TrackTest < ActiveSupport::TestCase
   #   assert_equal urls.size, 1
   #   assert_equal urls[0], "http://maps.google.com/?t=p&z=15&ll=37.78953170776367,-122.44117736816406&q=http://api.motionxlive.com/motionx-remote/api/gps/host/a75f5ef1-8c3a-4fff-8c5f-4752d9658c2e"
   # end
+  def setup
+    User.all.each{|u| u.destroy}
+  end
+
+  test "mailman should be able to parse user from email address" do
+    user = User.create(:name => "Bilbo Baggins", :email => "my@bobo.com", :password => "sekret", :password_confirmation => "sekret", :login => "bobo")
+    assert_equal Mailman.parse_user_from_address("tracks+bobo@something.com"), user
+  end
+
+  test "mailman should return nil if no user matches" do
+    assert_equal Mailman.parse_user_from_address("tracks+bobo@something.com"), nil
+  end
 end
