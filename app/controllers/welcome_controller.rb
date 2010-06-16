d14c7809d3a80f1b7e301a56e13a2f520b4098db
Nick Stielau <nick.stielau@gmail.com>
2010-06-16 02:30:47 -0700

    Showing graphs on user dashboard

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,6 +3,8 @@ class WelcomeController < ApplicationController
     @total_distance = current_user ? current_user.total_distance : nil
     @total_active_time = current_user ? current_user.total_active_time : nil
     @total_tracks = current_user ? current_user.tracks.size : nil
+
+    @user_data = TrackAggregator.new({:user_id => current_user.id, :interval => 'day', :start_time => 1.month.ago}).result
   end
 
   def help
