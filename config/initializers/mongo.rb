71d48a6cffbbadd9d691d52ad130fdbb66a6535f
Nick Stielau <nick.stielau@gmail.com>
2010-08-30 15:09:46 -0700

    Hack to work with the Mongo 1.6

--- config/initializers/mongo.rb
+++ config/initializers/mongo.rb
@@ -1,13 +1,13 @@
 require 'mongo_mapper'
 
 if Rails.env == "development"
-  MongoMapper.connection = Mongo::Connection.new
+  MongoMapper.connection = Mongo::Connection.new("127.0.0.1", 27017, :slave_ok => true)
   MongoMapper.database = "motionx_development"
 elsif Rails.env == "test"
-  MongoMapper.connection = Mongo::Connection.new
+  MongoMapper.connection = Mongo::Connection.new("127.0.0.1", 27017, :slave_ok => true)
   MongoMapper.database = "motionxdb_test"
 else
-  MongoMapper.connection = Mongo::Connection.new("pearl.local.mongohq.com", "27029")
+  MongoMapper.connection = Mongo::Connection.new("pearl.local.mongohq.com", "27029", :slave_ok => true)
   MongoMapper.database = "motionx_production"
   MongoMapper.database.authenticate(ENV['TRACKSTER_MONGOHQ_USERNAME'], ENV['TRACKSTER_MONGOHQ_PASSWORD'])
 end
