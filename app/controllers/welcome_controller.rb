405f05b3cf5c37d4b7c138e2c3c597382a647fdf
Nick Stielau <nick.stielau@gmail.com>
2010-09-10 16:57:41 -0700

    Adding additional graphs for twitter dashboard.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -4,13 +4,13 @@ class WelcomeController < ApplicationController
     @total_active_time = current_user ? current_user.total_active_time : nil
     @total_tracks = current_user ? current_user.tracks.size : nil
 
-    @user_data = TrackAggregator.new({:user_id => current_user.id, :interval => 'day', :start_time => 1.month.ago}).result
+    @user_data = TrackAggregator.new({:user_id => current_user ? current_user.id : nil, :interval => 'day', :start_time => 1.month.ago}).result
   end
 
   def help
   end
 
   def twitter_dashboard
-    @twitter_data = TwitterTrackAggregator.new({:interval => 'day', :start_time => 30.days.ago}).result
+    @twitter_data = TwitterTrackAggregator.new({:interval => 'day', :start_time => 60.days.ago}).result
   end
 end
